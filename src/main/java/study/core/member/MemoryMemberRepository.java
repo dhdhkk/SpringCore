eb6a6dd5c22006b90b76fe02c5d649d21607a322
KDH <dhdhkk3@gmail.com>
2022-11-14 18:11:19 +0900

    회원 도메인 개발까지

---
+++ src/main/java/study/core/member/MemoryMemberRepository.java
@@ -0,0 +1,22 @@
+package study.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // ConcurrentHashMap 사용해야함.. 동시성 이슈..
+
+    @Override
+    public void save(Member member) {
+
+        store.put(member.getId(), member);
+
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+
+        return store.get(memberId);
+    }
+}
