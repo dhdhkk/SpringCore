e2d711f56e03b23bcb9b52b336a76059f2c2c8db
kwon donghyun <dhdhkk3@gmail.com>
2022-12-03 18:27:02 +0900

    @Configuration과 바이트코드 조작(CGLIB)

--- src/test/java/study/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/study/core/singleton/ConfigurationSingletonTest.java
@@ -2,6 +2,7 @@ package study.core.singleton;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import study.core.AppConfig;
 import study.core.member.MemberRepository;
@@ -31,5 +32,26 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+        //출력: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$bd479d70
+
+        //이것은 내가 만든 클래스가 아니라 스프링이 CGLIB라는 바이트코드 조작 라이브러리를 사용해서
+        //AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고, 그 다른 클래스를 스프링 빈으로 등록한 것이다!
+        //@Bean이 붙은 메서드마다 이미 스프링 빈이 존재하면 존재하는 빈을 반환하고, 스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환하는 코드가 동적으로 만들어진다.
+        //덕분에 싱글톤이 보장되는 것이다.
+
+
+
+        //@Bean만 사용해도 스프링 빈으로 등록되지만, 싱글톤을 보장하지 않는다.
+        //memberRepository() 처럼 의존관계 주입이 필요해서 메서드를 직접 호출할 때 싱글톤을 보장하지 않는다.
+        //크게 고민할 것이 없다. 스프링 설정 정보는 항상 @Configuration 을 사용하자.
     }
 }
