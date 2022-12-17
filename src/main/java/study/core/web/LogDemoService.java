c17ff3f1e5c0bd93c1a19cee05e5684b703b669d
kwon donghyun <dhdhkk3@gmail.com>
2022-12-18 00:12:51 +0900

    스코프와 Provider

--- src/main/java/study/core/web/LogDemoService.java
+++ src/main/java/study/core/web/LogDemoService.java
@@ -1,15 +1,17 @@
 package study.core.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import study.core.common.MyLogger;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
